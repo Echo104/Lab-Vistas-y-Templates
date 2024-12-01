f0fd23c3d821940a5077fda05c1352190d804181
Kazel 10 <echoquequi@unsa.edu.pe>
2024-12-01 12:02:26 -0500

    Usando Templates para la pagina de inicio

--- reviews/ReviewLibro/views.py
+++ reviews/ReviewLibro/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-def myHomeView(*args,**kwargs):
-    return HttpResponse('<h1>Pagina de Inicio</h1>')
+def myHomeView(request):
+    return render(request,'reviews/inicio.html',)
